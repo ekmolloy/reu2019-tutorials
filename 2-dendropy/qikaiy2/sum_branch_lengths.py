fe457e8d4b6f547bbd4e5aca3f648b5b17538333
Yang <qikaiy2@golubh4.campuscluster.illinois.edu>
2019-06-05 17:58:11 -0500

    mission2

--- 2-dendropy/qikaiy2/sum_branch_lengths.py
+++ 2-dendropy/qikaiy2/sum_branch_lengths.py
@@ -8,20 +8,17 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("file", type=str, help="input_tree")
 args = parser.parse_args()
-print("the address of the tree file is as below:")
+print("\nthe address of the tree file is as below:")
 print(args.file)
-print("-------")
 tree = dendropy.Tree.get(path = args.file, schema = "newick", rooting = "force-rooted")
 store = tree.calc_node_root_distances(return_leaf_distances_only=False)
-print("the original tree is as below:")
+print("\nthe original tree is as below:")
 print(tree.as_string(schema="newick"))
-print("-------")
 #----------------------------------above is the first part -> get the tree and all the distances------------------------------
 i = 0
 for node in tree.preorder_node_iter():
     node.edge.length = store[i]
     i += 1
-print("the editted tree is as below:")
+print("\nthe editted tree is as below:")
 print(tree.as_string(schema="newick"))
-print("-------")
 #----------------------------------second part -> change the weights of the edges--------------------------------------------- 
