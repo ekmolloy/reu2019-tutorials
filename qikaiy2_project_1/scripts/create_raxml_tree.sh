9fcf5225bce0f93ccc4914fafa748ef1f075db16
QikaiYang <qikaiy2@illinois.edu>
2019-06-22 18:35:37 -0500

    project

--- qikaiy2_project_1/scripts/create_raxml_tree.sh
+++ qikaiy2_project_1/scripts/create_raxml_tree.sh
@@ -2,5 +2,5 @@ path=../input_data/aligns/tackle_data
 files=$(ls $path)
 for f in $files
 do
-	./raxmlHPC-PTHREADS-SSE3 -T 4 -s $path/$f -w /home/qikaiy2/Downloads/qikaiy2_project_1/output_data/trees/GTRCAT -m GTRCAT -V -p 12345 -n $f
+	./raxmlHPC-PTHREADS-SSE3 -T 4 -s $path/$f -w /home/qikaiy2/Downloads/reu2019-tutorials/qikaiy2_project_1/output_data/trees/GTRCAT -m GTRCAT -V -p 12345 -n $f
 done
