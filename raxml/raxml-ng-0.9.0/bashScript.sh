9aa4c936074b70670d6015ded83b7e374be8f98f
Cheng <binghui2@golubh4.campuscluster.illinois.edu>
2019-06-26 14:33:45 -0500

    some changes

--- raxml/raxml-ng-0.9.0/bashScript.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-##randomly chosen
-#filename="../astral/292RBBH_TRINITY_DN72_c0_g1_i1.phy"
-mycommand="./raxml-ng --msa ../astral/genePHYLIPs/292RBBH_TRINITY_DN72_c0_g1_i1.phy --prefix treeResults/output --model GTR+G --bs-trees 100 -threads 2"
-eval $mycommand
--- raxml/raxml-ng-0.9.0/bashScript_Matrix.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-fastme-2.1.5-linux64 -i EstimatedSeq_TRUE.phy -O results/estimatedMatrix.txt -d p
-fastme-2.1.5-linux64 -i ../astral/genePHYLIPs/292RBBH_TRINITY_DN72_c0_g1_i1.phy -O results/trueMatrix -d p
