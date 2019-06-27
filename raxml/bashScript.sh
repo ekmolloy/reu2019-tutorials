b9e15e1c45dfb2696608e93714069461b3251a46
Cheng <binghui2@golubh4.campuscluster.illinois.edu>
2019-06-26 21:25:00 -0500

    without rooting the trees

---
+++ raxml/bashScript.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+##randomly chosen
+#filename="../astral/292RBBH_TRINITY_DN72_c0_g1_i1.phy"
+#softwarePath="../../../reu2019/software/standard-RAxML-8.2.12/raxmlHPC-PTHREADS-SSE3"
+#resPath="/projects/tallis/binghui2/reu2019-tutorials/raxml/treeResults"
+#for file in uce_phylip/*
+#do
+#	filename=$(echo $file | cut -d'/' -f 2)	
+#	mycommand="./raxmlHPC-PTHREADS-SSE3 -s $file -p 12345 -w $resPath -m GTRGAMMA -T 8 -n $filename"
+#	eval $mycommand
+#done
+resPath="/projects/tallis/binghui2/reu2019-tutorials/raxml/RAxML_Results"
+for file in uce_phylip/*
+do 
+	filename=$(echo $file | cut -d'.' -f 1)
+	realname=$(echo $filename | cut -d'/' -f 2)
+	mycommand="./raxmlHPC-PTHREADS-SSE3 -s $file -p 12345 -w $resPath/$realname -m GTRGAMMA -T 12 -n $realname"
+	eval $mycommand
+	#mkdir $resPath/$realname
+done
