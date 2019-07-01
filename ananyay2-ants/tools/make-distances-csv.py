373d9fc7356fbc429832cd7e55027b3b41741fd4
Ananya <ayammanuru@gmail.com>
2019-07-01 12:57:35 -0500

    calculate distances csv

--- ananyay2-ants/tools/make-distances-csv.py
+++ ananyay2-ants/tools/make-distances-csv.py
@@ -7,25 +7,20 @@ directory = sys.argv[1]
 repnums = []
 
 for f in os.listdir(os.fsencode(directory)):
-    filename = os.fsendcode(f)
-    if filename.endswith("est.m"):
-        repnums.append(filename[:-6])
+    f=f.decode('UTF-8')
+    if f.endswith("est.m"):
+        repnums.append(f[:-6])
 
 for repnum in repnums:
-    original = [s.strip() for s in open(str(repnum) + "_or.m", 'r')]
-    estimated = [s.strip() for s in open(str(repnum) + "_est.m", 'r')]
+    original = np.array([s.split() for s in open(directory+str(repnum) + "_or.m", 'r')])
+    estimated = np.array([s.split() for s in open(directory+str(repnum) + "_est.m", 'r')])
     o_cols = original[:,0]
     est_cols = estimated[:,0]
-    print(o_cols)
-    print(est_cols)
-    print(original)
-    print(estimated)
     
     for row in range(len(original)):
         for col in range(2+row,len(original[0])):
             o_rowname=original[row, 0]
-            o_colname=o_cols[col-1,0]
-            e_rownum=est_cols.index(o_rowname)
-            e_colnum=est_cols.index(o_colname) + 1
+            o_colname=o_cols[col-1]
+            e_rownum=np.where(est_cols == o_rowname)[0][0]
+            e_colnum=np.where(est_cols == o_colname)[0][0] + 1
             print(o_rowname+","+ o_colname+","+original[row,col]+","+estimated[e_rownum,e_colnum])
-
