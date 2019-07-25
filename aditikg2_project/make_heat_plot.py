6bf922958adfcef9e93071d8757d15e08bdece06
Aditi Ghosalkar <aditikg2@illinois.edu>
2019-07-25 10:46:15 -0500

    Heat plot added

---
+++ aditikg2_project/make_heat_plot.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import seaborn as sb
+import matplotlib.pyplot as plt
+import numpy as np
+
+data = pd.read_table('./output/heat_plot_out/Scincella/SELT_matrix.mat', delimiter=" ", skiprows=1, header=None, skipinitialspace=True, index_col=False)
+k = data.drop(data.shape[1] - 1, 1)
+k = k.drop(0, 1)
+
+heat_map = sb.heatmap(k, cmap="YlGnBu", cbar_kws={'label': 'p-distance between sequences'}, xticklabels=data[0], yticklabels=data[0])
+plt.xlabel("Scincella lateralis individuals")
+plt.ylabel("Scincella lateralis individuals")
+plt.show()
+
