fea330f459cce5d1cc4831e9d6f5ec5cdb55660d
Aditi Ghosalkar <aditikg2@illinois.edu>
2019-07-27 17:45:17 -0500

    More box plots

--- aditikg2_project/make_box_plot.py
+++ aditikg2_project/make_box_plot.py
@@ -5,9 +5,11 @@ import matplotlib.pyplot as plt
 def make_box_plot():
 
     source = "./output/raxml_out/Scincella/*.phylip"
+    source2 = "./output/raxml_out/Scincella/*.log"
 
     num_seq = []
     algn_len = []
+    gaps = []
 
     for f in glob.glob(source):
 
@@ -17,15 +19,35 @@ def make_box_plot():
         num_seq.append(int(a))
         algn_len.append(int(b))
 
+    for f in glob.glob(source2):
+
+        con = open(f, 'r')
+
+        lines = con.readlines()
+
+        for line in lines:
+
+            if "Gaps:" in line:
+                a,b,c = line.split(" ")
+                gaps.append(float(b)*100)
+                break
+
     sns.set_style("whitegrid")
     plt.figure(1)
     sns.boxplot(data=num_seq, orient="h")
     plt.xlabel("Number of sequences")
     plt.yticks([""])
+    plt.savefig("./output/data_info_out/Scincella/num_seq.pdf", tight_layout=True, format='pdf', dpi=300)
     plt.figure(2)
     sns.boxplot(data=algn_len, orient="h")
     plt.xlabel("Length of sequence alignments")
     plt.yticks([""])
+    plt.savefig("./output/data_info_out/Scincella/len_algn.pdf", tight_layout=True, format='pdf', dpi=300)
+    plt.figure(3)
+    sns.boxplot(data=gaps, orient="h")
+    plt.xlabel("Percent gaps in data")
+    plt.yticks([" "])
+    plt.savefig("./output/data_info_out/Scincella/gaps.pdf", tight_layout=True, format='pdf', dpi=300)
     plt.show()
 
 make_box_plot()
